65664d172dec1edfc640ed5c09b50950b22abb45
ap-- <andreas@poehlmann.io>
2018-06-07 14:55:18 +0200

    temporarily patch in select support

--- src/plugins/Dashboard/FileCard.js
+++ src/plugins/Dashboard/FileCard.js
@@ -32,16 +32,35 @@ module.exports = class FileCard extends Component {
   renderMetaFields (file) {
     const metaFields = this.props.metaFields || []
     return metaFields.map((field) => {
-      return <fieldset class="uppy-DashboardFileCard-fieldset">
-        <label class="uppy-DashboardFileCard-label">{field.name}</label>
-        <input class="uppy-c-textInput uppy-DashboardFileCard-input"
-          type="text"
-          data-name={field.id}
-          value={file.meta[field.id]}
-          placeholder={field.placeholder}
-          onkeyup={this.tempStoreMetaOrSubmit}
-          onkeydown={this.tempStoreMetaOrSubmit}
-          onkeypress={this.tempStoreMetaOrSubmit} /></fieldset>
+      if (field.type === 'select') {
+        const radioOptions = field.options.map((opt, i) => {
+          return <option key={i} value={opt.value} label={opt.label}>{opt.label}</option>
+        })
+        return <fieldset class="uppy-DashboardFileCard-fieldset">
+          <label class="uppy-DashboardFileCard-label">{field.name}</label>
+          <select class="uppy-c-textInput uppy-DashboardFileCard-input"
+            type="text"
+            data-name={field.id}
+            value={file.meta[field.id]}
+            onchange={this.tempStoreMetaOrSubmit}
+            onkeyup={this.tempStoreMetaOrSubmit}
+            onkeydown={this.tempStoreMetaOrSubmit}
+            onkeypress={this.tempStoreMetaOrSubmit}>
+            {radioOptions}
+          </select>
+        </fieldset>
+      } else {
+        return <fieldset class="uppy-DashboardFileCard-fieldset">
+          <label class="uppy-DashboardFileCard-label">{field.name}</label>
+          <input class="uppy-c-textInput uppy-DashboardFileCard-input"
+            type="text"
+            data-name={field.id}
+            value={file.meta[field.id]}
+            placeholder={field.placeholder}
+            onkeyup={this.tempStoreMetaOrSubmit}
+            onkeydown={this.tempStoreMetaOrSubmit}
+            onkeypress={this.tempStoreMetaOrSubmit}/></fieldset>
+      }
     })
   }
 
