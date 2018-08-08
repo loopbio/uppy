b59b6145ff3aa611b249b0a8b66155661f671b49
ap-- <andreas@poehlmann.io>
2018-08-08 19:19:22 +0200

    metafield: fix preselect

--- src/plugins/Dashboard/FileCard.js
+++ src/plugins/Dashboard/FileCard.js
@@ -36,12 +36,13 @@ module.exports = class FileCard extends Component {
         const radioOptions = field.options.map((opt, i) => {
           return <option key={i} value={opt.value} label={opt.label}>{opt.label}</option>
         })
+        const selected = (field.options.find(x => x.selected) || field.options[0]).value
         return <fieldset class="uppy-DashboardFileCard-fieldset">
           <label class="uppy-DashboardFileCard-label">{field.name}</label>
           <select class="uppy-c-textSelect uppy-DashboardFileCard-input"
             type="text"
             data-name={field.id}
-            value={file.meta[field.id]}
+            value={selected}
             onchange={this.tempStoreMetaOrSubmit}
             onkeyup={this.tempStoreMetaOrSubmit}
             onkeydown={this.tempStoreMetaOrSubmit}
